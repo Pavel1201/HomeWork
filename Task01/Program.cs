e1b7dfe3ea4ed998ecf2f7793676cccdad24156a
Pavel <p.a.dudarev@mail.ru>
2022-09-01 05:12:40 +0500

    Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа: ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int max = 0;
+
+if (numberA > numberB)
+{
+     max = numberA;
+     Console.WriteLine($"Большее число {max}");
+ 
+
+}
+else 
+{
+
+    max = numberB;
+    Console.WriteLine($"Большее число {max}");
+
+}
