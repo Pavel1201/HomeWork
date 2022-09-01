fa523f1e9fc744dd4805880c80724ddec51e8f6d
Pavel <p.a.dudarev@mail.ru>
2022-09-01 05:21:59 +0500

    Task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите три числа: ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;    
+}
+if(numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine($"Наибольшее число {max}");
