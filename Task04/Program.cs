d5f3f9757e3c455b28e1bc8ea364b8965306b7ad
Pavel <p.a.dudarev@mail.ru>
2022-09-01 05:44:49 +0500

    Task 04

---
+++ Task04/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+Console.Write("Четные числа: ");
+while(count <= N)
+{
+    if(count%2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+
+
+    count++;
+}
